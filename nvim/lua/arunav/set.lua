aeff75195f1a22ea4e754c9fb707e0b54ba37554
ArunavDey <arunav.dey@protonmail.com>
2024-08-20 20:35:26 +0530

    upkeep: update dots

--- nvim/lua/arunav/set.lua
+++ nvim/lua/arunav/set.lua
@@ -1,15 +1,21 @@
 local vim = vim
 
-vim.opt.wildmenu = true
-vim.opt.incsearch = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-vim.opt.termguicolors = true
-vim.opt.wrap = true
+vim.g.mapleader = " "
+vim.g.termguicolors = true
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
 vim.opt.textwidth = 80
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+vim.opt.clipboard = "unnamed"
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+vim.opt.hlsearch = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
+
+-- add sane default for neovim sets

f3d9ee24fcaeb0a616052f8d825975626fbfc69f
arunavdey <arunav.dey@protonmail.com>
2025-05-07 21:32:34 +0530

    feat(nvim): update dots

--- dot_config/nvim/lua/arunav/lazy.lua
+++ dot_config/nvim/lua/arunav/lazy.lua
@@ -13,6 +13,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  { "github/copilot.vim" },
   { "rebelot/kanagawa.nvim" },
   {
     "stevearc/oil.nvim",
--- dot_config/nvim/lua/arunav/lsp.lua
+++ dot_config/nvim/lua/arunav/lsp.lua
@@ -3,11 +3,11 @@ local vim = vim
 -- Server manager setup
 require("mason").setup()
 require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers {
-  function(server_name)
-    require("lspconfig")[server_name].setup {}
-  end
-}
+-- require("mason-lspconfig").setup_handlers {
+--   function(server_name)
+--     require("lspconfig")[server_name].setup {}
+--   end
+-- }
 
 -- Server setup
 local lspconfig_defaults = require('lspconfig').util.default_config
